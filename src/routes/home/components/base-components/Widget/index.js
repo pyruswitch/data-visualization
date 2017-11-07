3ad4212d825d2cc79fc08ae294f5990cd4ca639c
bin.wang <bin.wang@zuolin.com>
2017-11-07 14:24:41 +0800

    标题样式调整

--- src/routes/home/components/base-components/Widget/index.js
+++ src/routes/home/components/base-components/Widget/index.js
@@ -3,22 +3,22 @@ import './index.less';
 const prefixCls = 'eh-data-visualization';
 
 const Widget = ({ className = '', title, children }) => {
-    return (
-        <div className={`${prefixCls}-widget`}>
-            <div>
-                <h1>
-                    <span>{title}</span>
-                </h1>
-            </div>
-            <span className='lefttop' />
-            <span className='leftbottom' />
-            <span className='rightbottom' />
-            <span className='righttop' />
-            <div className={`content ${className}`}>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${prefixCls}-widget`}>
+      <div>
+        <h1>
+          <span>{title}</span>
+        </h1>
+      </div>
+      <span className='lefttop' />
+      <span className='leftbottom' />
+      <span className='rightbottom' />
+      <span className='righttop' />
+      <div className={`content ${className}`}>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
