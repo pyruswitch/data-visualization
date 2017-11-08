390dc4fb140c5e7c35c7d8a5a5225d1bfedf3d95
bin.wang <bin.wang@zuolin.com>
2017-11-08 16:29:56 +0800

    增加图例的默认配置

--- src/routes/home/config.js
+++ src/routes/home/config.js
@@ -4,13 +4,17 @@ const fill = '#7D98EA';
 const margin = [10, 20, 30, 50];
 const stroke = "#5f79c5";
 export default {
+  // 统一高度，根据UI稿子计算，如果第一行公式没看懂，请联系Aha
   height,
+  // 默认的统一边距
   margin,
+  // title默认样式
   title: {
     fontSize: '8',
     textAlign: 'center',
     fill,
   },
+  // label默认样式
   labels: {
     autoRotate: false,
     label: {
@@ -19,7 +23,18 @@ export default {
       fontSize: '8',
     }
   },
-  // 设置坐标轴线的样式，如果值为 null，则不显示坐标轴线 图形属性
+  // 图例默认样式
+  legend: {
+    title: null,
+    unChecked: '#CCC',
+    wordSpaceing: 2,
+    itemWrap: false,
+    word: {
+      fill: '#999',
+      fontSize: 8
+    }
+  },
+  // 设置坐标轴线的样式
   line: {
     lineWidth: 1, // 设置线的宽度
     stroke //  设置线的颜色
