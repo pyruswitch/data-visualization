7cf3e7fcd35a3619a17dae7c0752a7877152e426
weining <vincent.wei@zuolin.com>
2018-06-11 16:47:59 +0800

    add namespace'data

--- datavbizdb.py
+++ datavbizdb.py
@@ -1862,5 +1862,5 @@ SELECT
 if __name__ == '__main__':
     #list1,list2,list3,list4,list5,list6=incomedata()
     #print(list1,'\n',list2,'\n',list3,'\n',list4,'\n',list5,'\n',list6)
-    list1=totaldiscount(999990)
+    list1=repurchaserate(999990)
     print(list1)
